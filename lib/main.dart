8674e85d6ac1bbdc25c37a8c83ebbc3c31ce9282
CrisKulczynsku <cristinasdk21@gmail.com>
2022-10-28 14:01:45 -0300

    Ajuste da main.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:immune/screens/tela_logo_inicial.dart';
+import 'package:immune/screens/tela_1_logo_inicial.dart';
 
 void main() => runApp(const MyApp());
 
@@ -11,18 +11,12 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        /*Cores padrão*/
+/*Cores padrão*/
+
         primaryColor: const Color.fromRGBO(90, 78, 190, 0.1),
         appBarTheme: const AppBarTheme(color: Color.fromRGBO(32, 37, 141, 1)),
-        buttonTheme: const ButtonThemeData(
-          buttonColor: Color.fromRGBO(32, 37, 141, 1),
-          textTheme: ButtonTextTheme.primary,
-        ),
-        colorScheme:
-        ColorScheme.fromSwatch().copyWith(secondary: const Color.fromRGBO(32, 37, 141, 1)),
       ),
       home: const TelaLogoInicial(),
     );
   }
 }
-
